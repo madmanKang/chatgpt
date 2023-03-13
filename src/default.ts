2df20fb9f5e6cd9f838e11450cee07e13485609b
madmanKang <286216393@qq.com>
2023-03-14 00:35:56 +0800

    Add files via upload

---
+++ src/default.ts
@@ -0,0 +1,19 @@
+export const defaultSetting = {
+  continuousDialogue: true,
+  archiveSession: false,
+  openaiAPIKey: "",
+  openaiAPITemperature: 60,
+  systemRule: ""
+}
+
+export const defaultMessage = `
+
+- 特感谢 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 和 [Vercel](http://vercel.com/) 提供支持。
+
+- 本站由 [不忘初心](https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzIxNDU2NTQ0NA==&scene=124#wechat_redirect) 搭建。
+
+- 想要部署的朋友，可关注公众号：**java空间**，添加我个人微信获取保姆级部署文档。
+
+- 关注公众号：**java空间**，回复关键字key,获取到最新的AppKey。
+
+- **Shift+Enter** 换行。开头输入 **/** 或者 **空格** 搜索 Prompt 预设。点击输入框滚动到底部。`
