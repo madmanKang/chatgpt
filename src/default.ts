f8d8bf6bcc9a1c0337e5bfcc3bff368c838c9ef0
madmanKang <286216393@qq.com>
2023-03-15 16:49:40 +0800

    文本修改

--- src/default.ts
+++ src/default.ts
@@ -10,8 +10,8 @@ export const defaultMessage = `
 
 - 特感谢 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 和 [Vercel](http://vercel.com/) 提供支持。
 
-- 本站由 [不忘初心](https://xad5z27lkeq.feishu.cn/docx/U88LdZbiBoyyzQxCiQkc7CconBh) 搭建。
+- 本站由 [不忘初心]搭建，扫描上方二维码添加公众号。
 
-- 搜索微信"g01014011"或者扫描上方二维码添加我的个人微信，获取到最新的AppKey。
+- 公众号回复“key”，获取OpenAI API Key。
 
 - **Shift+Enter** 换行。开头输入 **/** 或者 **空格** 搜索 Prompt 预设。点击输入框滚动到底部。`
